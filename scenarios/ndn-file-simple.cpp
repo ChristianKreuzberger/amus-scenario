bf142fccb76c8e7795ced4fa2dc19d2bae75432e
ChristianKreuzberger <christian.kreuzberger@itec.aau.at>
2015-03-23 22:14:28 +0100

    added file example

---
+++ scenarios/ndn-file-simple.cpp
@@ -0,0 +1,150 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+/**
+ * Copyright (c) 2015 - Christian Kreuzberger - based on ndnSIM
+ *
+ * This file is part of amus-ndnSIM. See AUTHORS for complete list of ndnSIM authors and
+ * contributors.
+ *
+ * ndnSIM is free software: you can redistribute it and/or modify it under the terms
+ * of the GNU General Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later version.
+ *
+ * ndnSIM is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ * PURPOSE.  See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * ndnSIM, e.g., in COPYING.md file.  If not, see <http://www.gnu.org/licenses/>.
+ **/
+
+// ndn-file-simple.cpp
+
+#include "ns3/core-module.h"
+#include "ns3/network-module.h"
+#include "ns3/point-to-point-module.h"
+#include "ns3/ndnSIM-module.h"
+#include "ns3/ndnSIM/apps/ndn-app.hpp"
+
+namespace ns3 {
+
+/**
+ * This scenario simulates a very simple network topology:
+ *
+ *
+ *      +----------+     1Mbps      +--------+     1Mbps      +----------+
+ *      | consumer | <------------> | router | <------------> | producer |
+ *      +----------+         10ms   +--------+          10ms  +----------+
+ *
+ *
+ * Consumer requests a file from a file server
+ * (interests contain constantly increasing sequence number).
+ *
+ * For every received interest, producer replies with a data packet, containing
+ * 1400 bytes of actual payload (MTU: 1500 by default).
+ *
+ * To run scenario and see what is happening, use the following command:
+ *
+ *     NS_LOG=ndn.FileServer:ndn.FileConsumer ./waf --run=ndn-file-simple
+ */
+
+
+void
+FileDownloadedTrace(Ptr<ns3::ndn::App> app, shared_ptr<const ndn::Name> interestName, double downloadSpeed, long milliSeconds)
+{
+  std::cout << "Trace: File finished downloading: " << Simulator::Now().GetMilliSeconds () << " "<< *interestName <<
+     " Download Speed: " << downloadSpeed << " bytes/s in " << milliSeconds << " ms" << std::endl;
+}
+
+void
+FileDownloadedManifestTrace(Ptr<ns3::ndn::App> app, shared_ptr<const ndn::Name> interestName, long fileSize)
+{
+  std::cout << "Trace: Manifest received: " << Simulator::Now().GetMilliSeconds () <<" "<< *interestName << " File Size: " << fileSize << std::endl;
+}
+
+void
+FileDownloadStartedTrace(Ptr<ns3::ndn::App> app, shared_ptr<const ndn::Name> interestName)
+{
+  std::cout << "Trace: File started downloading: " << Simulator::Now().GetMilliSeconds () <<" "<< *interestName << std::endl;
+}
+
+int
+main(int argc, char* argv[])
+{
+  // setting default parameters for PointToPoint links and channels
+  Config::SetDefault("ns3::PointToPointNetDevice::DataRate", StringValue("10Mbps"));
+  Config::SetDefault("ns3::PointToPointChannel::Delay", StringValue("10ms"));
+  Config::SetDefault("ns3::DropTailQueue::MaxPackets", StringValue("20"));
+
+  // Read optional command-line parameters (e.g., enable visualizer with ./waf --run=<> --visualize
+  CommandLine cmd;
+  cmd.Parse(argc, argv);
+
+  // Creating nodes
+  NodeContainer nodes;
+  nodes.Create(4);
+
+  // Connecting nodes using two links
+  PointToPointHelper p2p;
+  p2p.Install(nodes.Get(0), nodes.Get(2));
+  p2p.Install(nodes.Get(1), nodes.Get(2));
+  p2p.Install(nodes.Get(2), nodes.Get(3));
+
+
+
+  // Install NDN stack on all nodes
+  ndn::StackHelper ndnHelper;
+  ndnHelper.SetDefaultRoutes(true);
+  ndnHelper.InstallAll();
+
+  // Choosing forwarding strategy
+  ndn::StrategyChoiceHelper::InstallAll("/some_prefix", "/localhost/nfd/strategy/broadcast");
+
+  // Installing applications
+  // HINT: Create a directory where you put your stuff, e.g.: /home/ckreuz/dataDir
+  // then create a large file, using fallocate -l 10M test.img
+
+  // Consumer
+  ndn::AppHelper consumerHelper("ns3::ndn::FileConsumerCbr");
+  // Consumer will request /prefix/0, /prefix/1, ...
+  consumerHelper.SetAttribute("FileToRequest", StringValue("/some_prefix/test.img"));
+  //consumerHelper.SetAttribute("WriteOutfile", StringValue("/home/ckreuz/index.out.html"));
+  consumerHelper.SetAttribute("MaxPayloadSize", StringValue("1400"));
+
+  // Start one consumer at 1.0 seconds, the other consumer at 2.0 seconds
+  consumerHelper.Install(nodes.Get(0));
+  consumerHelper.Install(nodes.Get(1));
+
+
+  // Connect Traces
+  Config::ConnectWithoutContext("/NodeList/*/ApplicationList/*/FileDownloadFinished",
+                               MakeCallback(&FileDownloadedTrace));
+  Config::ConnectWithoutContext("/NodeList/*/ApplicationList/*/ManifestReceived",
+                               MakeCallback(&FileDownloadedManifestTrace));
+  Config::ConnectWithoutContext("/NodeList/*/ApplicationList/*/FileDownloadStarted",
+                               MakeCallback(&FileDownloadStartedTrace));
+
+
+  // Producer
+  ndn::AppHelper producerHelper("ns3::ndn::FileServer");
+
+  // Producer will reply to all requests starting with /prefix
+  producerHelper.SetPrefix("/some_prefix");
+  producerHelper.SetAttribute("ContentDirectory", StringValue("/home/ckreuz/dataDir/"));
+  producerHelper.SetAttribute("MaxPayloadSize", StringValue("1400"));
+  producerHelper.Install(nodes.Get(3)); // last node
+
+  Simulator::Stop(Seconds(60.0));
+
+  Simulator::Run();
+  Simulator::Destroy();
+
+  return 0;
+}
+
+} // namespace ns3
+
+int
+main(int argc, char* argv[])
+{
+  return ns3::main(argc, argv);
+}
